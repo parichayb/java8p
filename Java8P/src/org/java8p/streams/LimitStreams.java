3fbf4986958aa1685b465892b89be3b2d4bd0fd0
Parichay Banerjee <gotopbmail@gmail.com>
2020-05-13 21:18:42 +0530

    streams test

--- Java8P/src/org/java8p/streams/LimitStreams.java
+++ Java8P/src/org/java8p/streams/LimitStreams.java
@@ -1,8 +1,10 @@
 package org.java8p.streams;
 
+import java.util.stream.Stream;
+
 public class LimitStreams {
 
 	public static void main(String[] args) {
-		
+		Stream<Integer> x = Stream.iterate(1, t-> t+1);
 	}
 }
